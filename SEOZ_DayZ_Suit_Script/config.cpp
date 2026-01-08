c7a69141459d69efd1e26ded0fe6c444bb1c4b05
ArthurHutchinson <112464170+ArthurHutchinson@users.noreply.github.com>
2026-01-08 13:33:00 -0500

    Jacket Vest Switch
    Jackets will be the main class instead of the vest as some suits do not carry vests.
    
    Eco's suits have been switched from backpacks to jackets as main class.

--- SEOZ_DayZ_Suit_Script/config.cpp
+++ SEOZ_DayZ_Suit_Script/config.cpp
@@ -62,8 +62,29 @@ class CfgVehicles
 		// FOR BULAT SUIT USE SEVA SUIT NOT PS5M
 	// TODO: Organize by suit type, not faction.
 
-	class SFM_SunriseVestFull_1;
+	class SFM_SunriseJacket1;
+	class SFM_SevaJacketV1;
+	class SFM_BerilJacket_Base;
+	class SFM_BerilVest_Stalker;
+	class SFM_VestMerc_Stalker;
+	class SFM_SSP99M_Jacket_V1_Red;
+	class SFM_SSP99M_Jacket_V2_Green;
+	class SFMC_SunriseJacket_Duty;
+	class SFM_SevaJacketV1_Dolg1;
+	class SFM_PS5MS_Jacket_Neitral1;
+	class SFM_SASJacket5;
+	class SFM_SASJacket4;
+	class SFM_PS5MS_Jacket_Dolg1;
+	class sfm_sevajacketv1_freedumb;
+	class SFM_PS5MS_Jacket_Freedumb;
+	class SFM_ZakatJacket_Freedumb;
+	class SFM_WFJacket_Freedumb;
+	class SFM_PS5MS_Jacket_IPSF;
+	class SFM_SevaJacketV1_IPSF;
 	class SFM_SevaVestFull;
+	class SFM_SASJacket_Freedumb;
+	class SFM_SevaVestFull_Dolg1;
+	class SFM_SevaVestFull_Svoboda1;
 	class SFM_BerilVest_Stalker;
 	class SFM_VestMerc_Stalker;
 	class SFM_SSP99M_Back_Red;
@@ -91,35 +112,35 @@ class CfgVehicles
 	 // LONER //
 	///////////
 
-	class SFM_Suit_SunriseFull_1: SFM_SunriseVestFull_1
+	class SFM_Suit_SunriseFull_1: SFM_SunriseJacket1
 	{
 		scope = 2;
 		displayName = "Sunrise Suit";
-		suit[] = {"SFM_SunriseJacket1","SFM_SunrisePants1"};
-		suitSlots[] = {"Body", "Legs"};
+		suit[] = {"SFM_SunriseVestFull_1","SFM_SunrisePants1"};
+		suitSlots[] = {"Vest", "Legs"};
 	};
-	class SFM_Suit_SunriseFull_1_Gas: SFM_SunriseVestFull_1
+	class SFM_Suit_SunriseFull_1_Gas: SFM_SunriseJacket1
 	{
 		scope = 2;
 		displayName = "Sunrise Suit (Gas Tank)";
-		suit[] = {"SFM_SunriseJacket1","SFM_SunrisePants1","SFM_SunriseGasBag"};
-		suitSlots[] = {"Body", "Legs","Back"};
+		suit[] = {"SFM_SunriseVestFull_1","SFM_SunrisePants1","SFM_SunriseGasBag"};
+		suitSlots[] = {"Vest", "Legs","Back"};
 	};
 
-	class SFM_Suit_SevaFull: SFM_SevaVestFull
+	class SFM_Suit_SevaFull: SFM_SevaJacketV1
 	{
 		scope = 2;
 		displayName = "Seva Suit";
-		suit[] = {"SFM_SevaJacketV1","SFM_SevaPants","MilitaryBoots_Black","OMNOGloves_Gray","SFM_SevaGlass","SFM_SevaBag"};
-		suitSlots[] = {"Body","Legs","Feet","Gloves","Mask","Back"};
+		suit[] = {"SFM_SevaVestFull","SFM_SevaPants","MilitaryBoots_Black","OMNOGloves_Gray","SFM_SevaGlass","SFM_SevaBag"};
+		suitSlots[] = {"Vest","Legs","Feet","Gloves","Mask","Back"};
 	};
 	
-	class SFM_Suit_Zakat: SFM_ZakatVest2_1
+	class SFM_Suit_Zakat: SFM_ZakatJacket1
 	{
 		scope = 2;
 		displayName = "Zakat Suit";
-		suit[] = {"SFM_ZakatJacket1","SFM_ZakatPants1"};
-		suitSlots[] = {"Body","Legs"};
+		suit[] = {"SFM_ZakatVest2_1","SFM_ZakatPants1"};
+		suitSlots[] = {"Vest","Legs"};
 	};
 	
 	class SFM_Suit_WFJacket: SFM_WFJacket_Stalker1
@@ -130,44 +151,44 @@ class CfgVehicles
 		suitSlots[] = {"Legs"};
 	};
 	
-	class SFM_WFVest: SFM_WFVest_Green
+	class SFM_WFVest: SFM_WFJacket_Stalker1
 	{
 		scope = 2;
 		displayName = "Wind of Freedom (loner) Suit";
-		suit[] = {"SFM_WFJacket_Stalker1","SFM_WFPants_Stalker"};
-		suitSlots[] = {"Body", "Legs"};
+		suit[] = {"SFM_WFVest_Green","SFM_WFPants_Stalker"};
+		suitSlots[] = {"Vest", "Legs"};
 	};
 	
-	class SFM_Suit_SAS: SFM_ZakatVest3_1
+	class SFM_Suit_SAS: SFM_SASJacket5
 	{
 		scope = 2;
 		displayName = "SAS Suit";
-		suit[] = {"SFM_SASJacket5","SFM_SASPants5","MilitaryBoots_Black"};
-		suitSlots[] = {"Body","Legs","Feet"};
+		suit[] = {"SFM_ZakatVest3_1","SFM_SASPants5","MilitaryBoots_Black"};
+		suitSlots[] = {"Vest","Legs","Feet"};
 	};
 	
-	class SFM_Suit_SAS_2: SFM_ZakatVest3_1
+	class SFM_Suit_SAS_2: SFM_SASJacket4
 	{
 		scope = 2;
 		displayName = "SAS Suit";
-		suit[] = {"SFM_SASJacket4","SFM_SASPants4","MilitaryBoots_Black"};
-		suitSlots[] = {"Body","Legs","Feet"};
+		suit[] = {"SFM_ZakatVest3_1","SFM_SASPants4","MilitaryBoots_Black"};
+		suitSlots[] = {"Vest","Legs","Feet"};
 	};
 	
-	class SFM_Suit_SAS_Bag: SFM_ZakatVest3_1
+	class SFM_Suit_SAS_Bag: SFM_SASJacket5
 	{
 		scope = 2;
 		displayName = "SAS Suit (Mule)";
-		suit[] = {"SFM_SASJacket5","SFM_SASPants5","MilitaryBoots_Black","SFM_RaiderBags_Green"};
-		suitSlots[] = {"Body","Legs","Feet","back"};
+		suit[] = {"SFM_ZakatVest3_1","SFM_SASPants5","MilitaryBoots_Black","SFM_RaiderBags_Green"};
+		suitSlots[] = {"Vest","Legs","Feet","back"};
 	};
 	
-	class SFM_Suit_Beril: SFM_BerilVest_Voen1
+	class SFM_Suit_Beril: SFM_PS5MS_Jacket_Neitral1
 	{
 		scope = 2;
 		displayName = "Beril Suit";
-		suit[] = {"SFM_PS5MS_Jacket_Neitral1","SFM_PS5MS_Pants_Neitral1","MilitaryBoots_Black","SFM_BulatHelmet"};
-		suitSlots[] = {"Body","Legs","Feet","Headgear","back"};
+		suit[] = {"SFM_BerilVest_Voen1","SFM_PS5MS_Pants_Neitral1","MilitaryBoots_Black","SFM_BulatHelmet"};
+		suitSlots[] = {"Vest","Legs","Feet","Headgear","back"};
 	};
 
 	// TODO: Make the shirt using Berill_shirt_stalker.paa
@@ -195,24 +216,24 @@ class CfgVehicles
 	///////////////
 
 
-	class SFM_Suit_SSP99M_Red: SFM_SSP99M_Back_Red
+	class SFM_Suit_SSP99M_Red: SFM_SSP99M_Jacket_V1_Red
 	{
 		// TODO: Add in the null/invisible slot for the 'vest' (NH should have one). Reason why is because when using the suit vests above will dupe the pants/jacket and drop it on the ground.
 		scope = 2;
 		displayName = "Ecologist Suit (Red)";
-		suit[] = {"SFM_SSP99M_Jacket_V1_Red","SFM_SSP99M_Pants_Red","SFM_SSP99M_Boots","SFM_SSP99M_Gloves","SFM_SevaGlass"};
-		suitSlots[] = {"Body","Legs","Feet","Gloves","Mask"};
+		suit[] = {"SFM_SSP99M_Back_Red","SFM_SSP99M_Pants_Red","SFM_SSP99M_Boots","SFM_SSP99M_Gloves","SFM_SevaGlass"};
+		suitSlots[] = {"Back","Legs","Feet","Gloves","Mask"};
 	};
 
 	// TODO: Needs to be tested.
 	
-	class SFM_Suit_SSP99M_Green: SFM_SSP99M_Back_Green
+	class SFM_Suit_SSP99M_Green: SFM_SSP99M_Jacket_V2_Green
 	{
 		// TODO: Add in the null/invisible slot for the 'vest' (NH should have one). Reason why is because when using the suit vests above will dupe the pants/jacket and drop it on the ground.
 		scope = 2;
 		displayName = "Ecologist Suit (Green)";
-		suit[] = {"SFM_SSP99M_Jacket_V2_Green","SFM_SSP99M_Pants_Green","SFM_SSP99M_Boots","SFM_SSP99M_Gloves","SFM_SevaGlass"};
-		suitSlots[] = {"Body","Legs","Feet","Gloves","Mask"};
+		suit[] = {"SFM_SSP99M_Back_Green","SFM_SSP99M_Pants_Green","SFM_SSP99M_Boots","SFM_SSP99M_Gloves","SFM_SevaGlass"};
+		suitSlots[] = {"Back","Legs","Feet","Gloves","Mask"};
 	};
 	
 	class SFP_Suit_HazOP: SFP_Hazmat_tors
@@ -231,36 +252,36 @@ class CfgVehicles
 	 // DUTY //
 	//////////
 
-	class SFM_Suit_Duty_Sunrise: SFMC_SunriseVestFull_Duty_1
+	class SFM_Suit_Duty_Sunrise: SFMC_SunriseJacket_Duty
 	{
 		scope = 2;
 		displayName = "Sunrise Suit (Duty)";
-		suit[] = {"SFMC_SunriseJacket_Duty","SFMC_SunrisePants_Duty"};
-		suitSlots[] = {"Body", "Legs"};
+		suit[] = {"SFMC_SunriseVestFull_Duty_1","SFMC_SunrisePants_Duty"};
+		suitSlots[] = {"Vest", "Legs"};
 	};
 	
-	class SFM_Suit_Duty_Sunrise_Gas: SFMC_SunriseVestFull_Duty_1
+	class SFM_Suit_Duty_Sunrise_Gas: SFMC_SunriseJacket_Duty
 	{
 		scope = 2;
 		displayName = "Sunrise Suit (Duty) (Gas Tank)";
-		suit[] = {"SFMC_SunriseJacket_Duty","SFMC_SunrisePants_Duty","SFMC_SunriseGasBag_Duty"};
-		suitSlots[] = {"Body", "Legs","Back"};
+		suit[] = {"SFMC_SunriseVestFull_Duty_1","SFMC_SunrisePants_Duty","SFMC_SunriseGasBag_Duty"};
+		suitSlots[] = {"Vest", "Legs","Back"};
 	};
 
-	class SFM_Suit_Duty_Seva: SFM_SevaVestFull_Dolg1
+	class SFM_Suit_Duty_Seva: SFM_SevaJacketV1_Dolg1
 	{
 		scope = 2;
 		displayName = "Seva Suit (Duty)";
-		suit[] = {"SFM_SevaJacketV1_Dolg1","SFM_SevaPants_Dolg1","MilitaryBoots_Black","OMNOGloves_Gray","SFM_SevaGlass","dzrp_Seva5M_Set_Dolg_GasBag"};
-		suitSlots[] = {"Body","Legs","Feet","Gloves","Mask","Back"};
+		suit[] = {"SFM_SevaVestFull_Dolg1","SFM_SevaPants_Dolg1","MilitaryBoots_Black","OMNOGloves_Gray","SFM_SevaGlass","dzrp_Seva5M_Set_Dolg_GasBag"};
+		suitSlots[] = {"Vest","Legs","Feet","Gloves","Mask","Back"};
 	};
 	
-	class SFM_Suit_Bulat_Dolg: SFM_Bulat_Vest_Dolg
+	class SFM_Suit_Bulat_Dolg: SFM_PS5MS_Jacket_Dolg1
 	{
 		scope = 2;
 		displayName = "Seva Suit (Duty)";
-		suit[] = {"SFM_PS5MS_Jacket_Dolg1","SFM_PS5MS_Pants_Dolg1","MilitaryBoots_Black","SFM_BulatHelmet_Dolg1"};
-		suitSlots[] = {"Body","Legs","Feet","Headgear"};
+		suit[] = {"SFM_Bulat_Vest_Dolg","SFM_PS5MS_Pants_Dolg1","MilitaryBoots_Black","SFM_BulatHelmet_Dolg1"};
+		suitSlots[] = {"Vest","Legs","Feet","Headgear"};
 	};
 
 	// TODO: Make Suit Item for Duty Beril
@@ -274,45 +295,45 @@ class CfgVehicles
 	// TODO: Make Suit Item for Freedom Sunrise
 		// TODO: Make the invidiual items for Freedom Sunrise
 
-	class SFM_Suit_Freedom_Seva: SFM_SevaVestFull_Svoboda1
+	class SFM_Suit_Freedom_Seva: sfm_sevajacketv1_freedumb
 	{
 		scope = 2;
 		displayName = "Seva Suit (Freedom)";
-		suit[] = {"sfm_sevajacketv1_freedumb","SFM_SevaPants_Freedumb","MilitaryBoots_Black","OMNOGloves_Gray","SFM_SevaGlass","SFM_SevaBag_Svoboda"};
-		suitSlots[] = {"Body","Legs","Feet","Gloves","Mask","Back"};
+		suit[] = {"SFM_SevaVestFull_Svoboda1","SFM_SevaPants_Freedumb","MilitaryBoots_Black","OMNOGloves_Gray","SFM_SevaGlass","SFM_SevaBag_Svoboda"};
+		suitSlots[] = {"Vest","Legs","Feet","Gloves","Mask","Back"};
 	};
 
 	// TODO: Make Suit Item for Freedom Beril
-	class SFM_Suit_Freedom_Beril: SFM_BerilVest_Svoboda1
+	class SFM_Suit_Freedom_Beril: SFM_PS5MS_Jacket_Freedumb
 	{
 		scope = 2;
 		displayName = "Berill Suit (Freedom)";
-		suit[] = {"SFM_PS5MS_Jacket_Freedumb","SFM_PS5MS_Pants_Freedumb","SFM_BerilHelmet_Svoboda1","SFM_GasMask_XM40"};
-		suitSlots[] = {"Body","Legs","Headgear","Mask"};
+		suit[] = {"SFM_BerilVest_Svoboda1","SFM_PS5MS_Pants_Freedumb","SFM_BerilHelmet_Svoboda1","SFM_GasMask_XM40"};
+		suitSlots[] = {"Vest","Legs","Headgear","Mask"};
 	};
 	
-	class SFM_Suit_ZakatVest_Freedom: SFM_ZakatVestFull_1
+	class SFM_Suit_ZakatVest_Freedom: SFM_ZakatJacket_Freedumb
 	{
 		scope = 2;
 		displayName = "Zakat Suit (Freedom)";
-		suit[] = {"SFM_ZakatJacket_Freedumb","SFM_ZakatPants_Freedumb"};
-		suitSlots[] = {"Body","Legs"};
+		suit[] = {"SFM_ZakatVestFull_1","SFM_ZakatPants_Freedumb"};
+		suitSlots[] = {"Vest","Legs"};
 	};
 	
-	class SFM_Suit_MTV_Freedom: SFM_MTV_Green
+	class SFM_Suit_MTV_Freedom: SFM_SASJacket_Freedumb
 	{
 		scope = 2;
 		displayName = "SAS Suit (Freedom)";
-		suit[] = {"SFM_SASJacket_Freedumb","SFM_SASPants_Freedumb"};
-		suitSlots[] = {"Body","Legs"};
+		suit[] = {"SFM_MTV_Green","SFM_SASPants_Freedumb"};
+		suitSlots[] = {"Vest","Legs"};
 	};
 	
-	class SFM_Suit_WFVest_Freedom: SFM_WFVest_Green
+	class SFM_Suit_WFVest_Freedom: SFM_WFJacket_Freedumb
 	{
 		scope = 2;
 		displayName = "Wind of Freedom";
-		suit[] = {"SFM_WFJacket_Freedumb","SFM_WFPants_Freedumb"};
-		suitSlots[] = {"Body","Legs"};
+		suit[] = {"SFM_WFVest_Green","SFM_WFPants_Freedumb"};
+		suitSlots[] = {"Vest","Legs"};
 	};
 
 	// TODO: Make Suit Item for Freedom Merc
@@ -334,28 +355,28 @@ class CfgVehicles
 
 	// TODO: TEST
 
-	class SFM_Suit_Military_Beril: SFM_BerilVest_Base
+	class SFM_Suit_Military_Beril: SFM_PS5MS_Jacket_IPSF
 	{
 		scope = 2;
 		displayName = "Berill Suit (Military)";
-		suit[] = {"SFM_PS5MS_Jacket_IPSF","SFM_PS5MS_Pants_IPSF","SFM_BerilHelmet_IPSF"};
-		suitSlots[] = {"Body","Legs","Headgear"};
+		suit[] = {"SFM_BerilVest_Base","SFM_PS5MS_Pants_IPSF","SFM_BerilHelmet_IPSF"};
+		suitSlots[] = {"Vest","Legs","Headgear"};
 	};
 
 	class SFM_Suit_Military_SAS: SFM_MTV_Black
 	{
 		scope = 2;
 		displayName = "SAS Suit (Military)";
-		suit[] = {"SFM_SASJacket_IPSF","SFM_SASPants_IPSF"};
-		suitSlots[] = {"Body","Legs"};
+		suit[] = {"SFM_MTV_Black","SFM_SASPants_IPSF"};
+		suitSlots[] = {"Vest","Legs"};
 	};
 
-	class SFM_Suit_Military_SEVA: SFM_SevaVest3
+	class SFM_Suit_Military_SEVA: SFM_SevaJacketV1_IPSF
 	{
 		scope = 2;
 		displayName = "SEVA Suit (Military)";
-		suit[] = {"SFM_SevaJacketV1_IPSF","SFM_SevaPants_IPSF","SFM_SevaGlass","SFM_SevaBag"};
-		suitSlots[] = {"Body","Legs","Mask","Back"};
+		suit[] = {"SFM_SevaVest3","SFM_SevaPants_IPSF","SFM_SevaGlass","SFM_SevaBag"};
+		suitSlots[] = {"Vest","Legs","Mask","Back"};
 	};
 
 	class SFM_Suit_Military_Zakat: SFM_ZakatJacket_IPSF
